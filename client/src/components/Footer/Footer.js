ee69f3aad2674b50c0a620488e27e4ffaed450f9
Tabish Naqvi <tabish.naqvi2003@gmail.com>
2023-03-04 07:35:47 +0530

    added footer

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import styles from './Footer.module.css'
 
 const Footer = () => {
   return (
-    <div>
-      Footer
+    <div className={styles.footerDiv}>
+      <h3>CulinaryCrave&copy; 2023 All Rights Reserved</h3>
+      <p>Founded by Tabish Naqvi</p>
     </div>
   )
 }
