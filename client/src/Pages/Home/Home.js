4fb147a371f3954fdf1e32799d2798bce241f047
Tabish Naqvi <tabish.naqvi2003@gmail.com>
2023-03-05 22:37:12 +0530

    added swiper on home page and changed styles

--- client/src/Pages/Home/Home.js
+++ client/src/Pages/Home/Home.js
@@ -1,21 +1,56 @@
 import React from 'react'
-import Navbar from '../../components/Navbar/Navbar'
 import styles from './Home.module.css'
-import ingred from '../../assets/ingred.png'
+import ing from '../../assets/ingred.png'
 import Grid from '../../components/Grid/Grid'
+import { useState } from 'react'
+import SwiperC from '../../components/Swiper/SwiperC'
 
 const Home = () => {
+
+  const [userName, setuserName] = useState('');
+  const [userEmail, setuserEmail] = useState('');
+  const [RecipeName, setRecipeName] = useState('');
+  const [ingred, setingred] = useState('');
+
+  const submitUser = async () => {
+    const response = await fetch('http://localhost:8181/submit', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify({
+        userName: userName,
+        userEmail: userEmail,
+        recipeName: RecipeName,
+        ingreds: ingred 
+      })
+    });
+
+    const json = await response.json();
+    console.log(json);
+    setuserName('')
+    setuserEmail('')
+    setRecipeName('')
+    setingred('')
+  }
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    await submitUser();
+  }
+
   return (
     <>
     <section className={styles.mainSection}>
       <div className={styles.mainSlogan}>
-        <img src={ingred} alt='ingredients' height={400} width={400} style={{
+        <img src={ing} alt='ingredients' height={400} width={400} style={{
           borderTopLeftRadius: '40px',
           borderBottomLeftRadius: '40px',
           position: 'absolute',
-          marginTop: '350px',
+          marginTop: '100px',
           zIndex: '0',
           right: '0',
+          boxShadow: '0px 0px 10px 1px grey'
         }}/>
         <p className={styles.mainSloganText}>Discover new flavors and techniques</p>
         <p className={styles.subSlogan}>Join Culinary Craze and Explore a World of Flavors</p>
@@ -24,12 +59,12 @@ const Home = () => {
         </div>
       </div>
     </section>
+    <section className={styles.SwiperRecip}>
+      <SwiperC />
+    </section>
     <section className={styles.gridSection}>
         <Grid />
         {/* add an image */}
-    </section>
-    <section className={styles.SwiperRecip}>
-      MYSWIPER
     </section>
     <section className={styles.publish}>
       <div className={styles.formDiv}>
@@ -37,15 +72,20 @@ const Home = () => {
         <h4 className={styles.formContent2}>And share it with the rest of the world</h4>
       </div>
       <div className={styles.frm}>
-        <form className={styles.formLanding} autoComplete='off'>
+        <form className={styles.formLanding} autoComplete='off' onSubmit={handleSubmit}>
+
           <label for='name'>Your Name</label>
-          <input type='text' name='name' required className={styles.formInp}/>
+          <input type='text' name='name' required className={styles.formInp} value={userName} onChange={event => setuserName(event.target.value)}/>
+
           <label for='email'>Your Email</label>
-          <input type='email' name='email' required className={styles.formInp}/>
+          <input type='email' name='email' required className={styles.formInp} value={userEmail} onChange={event =>setuserEmail(event.target.value)}/>
+
           <label for='recipeName'>Give your Recipe a Name</label>
-          <input type='text' name='recipeName'  required className={styles.formInp}/>
+          <input type='text' name='recipeName'  required className={styles.formInp} value={RecipeName} onChange={event => setRecipeName(event.target.value)}/>
+
           <label>What did you use?</label>
-          <input type='textarea' required className={styles.formInp}/>
+          <input type='textarea' required className={styles.formInp} value={ingred} onChange={event => setingred(event.target.value)}/>
+
           <button type='submit' className={styles.submitButton}>Publish!</button>
         </form>
       </div>
