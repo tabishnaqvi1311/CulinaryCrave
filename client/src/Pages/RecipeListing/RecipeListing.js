02c87bc0cc65a3913c06e693d71e03232b61772c
Tabish Naqvi <tabish.naqvi2003@gmail.com>
2023-03-06 05:00:23 +0530

    added recipeListing

--- client/src/Pages/RecipeListing/RecipeListing.js
+++ client/src/Pages/RecipeListing/RecipeListing.js
@@ -1,8 +1,35 @@
 import React from 'react'
-
+import styles from './RecipeListing.module.css';
+import { useState, useEffect } from 'react';
 const RecipeListing = () => {
+
+  const [myRecipes, setmyRecipes] = useState([]);
+
+  useEffect(()=>{
+    fetch('/api/recipes')
+      .then(response => response.json())
+      .then(data => setmyRecipes(data));  
+    }, []);
+    console.log(myRecipes);
+    
   return (
-    <div>RecipeListing</div>
+    <>
+    <div>
+      <h1 className={styles.topPicks}>Today's Top Picks</h1>
+      <div className={styles.threepicks}>
+        <div className={styles.pick1}>item1</div>
+        <div className={styles.pick2}>item2</div>
+        <div className={styles.pick3}>item3</div>
+      </div>
+    </div>
+    <hr className={styles.hori}/>
+    <div className={styles.recipeList}>
+        <div key={myRecipes._id} className={styles.recipeCard}>
+          <h2>{myRecipes.name}</h2>
+          <p>Rating: {myRecipes.rating}</p>
+        </div>
+    </div>
+    </>
   )
 }
 
