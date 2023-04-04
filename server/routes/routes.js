f6eefd140c9f6ccf54a2eda59a6509c80f28c47f
Tabish Naqvi <tabish.naqvi2003@gmail.com>
2023-04-04 22:42:18 +0530

    added /random route

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -4,7 +4,7 @@ const router = express.Router()
 const { getAllRecipes, getRecipe, createRecipe, deleteRecipe, getRandom } = require('../controller/recipeRoutes')
 
 router.route('/').get(getAllRecipes).post(createRecipe)
+router.route('/:random').get(getRandom)
 router.route('/:id').get(getRecipe).delete(deleteRecipe)
-router.route('/random').get(getRandom)
 
 module.exports = router
