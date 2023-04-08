7245c4f8e96b2f5da0ffdcfae49b155f1f5908cb
Tabish Naqvi <tabish.naqvi2003@gmail.com>
2023-04-08 19:04:23 +0530

    refactored landing page

--- client/src/Pages/Landing.jsx
+++ client/src/Pages/Landing.jsx
@@ -1,22 +1,8 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom'
-// import Search from './Search';
-import { useNavigate } from 'react-router-dom';
+import SearchBar from '../components/SearchBar';
 
 const Landing = () => {
-
-    const [searchTerm, setSearchTerm] = useState('');
-    const history = useNavigate()
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-
-        if(searchTerm){
-            history(`/search/${searchTerm}`)
-        }
-        setSearchTerm('');
-    }
-   
 
     return (
         <section className={`bg-swiper bg-no-repeat bg-cover lg:h-[100vh] h-[170vh] w-full`}>
@@ -28,8 +14,7 @@ const Landing = () => {
                 <div className='flex lg:flex-row flex-col items-center'>
                     <Link to='/recipes' className='bg-[#3c518b] p-4 text-white rounded-3xl hover:bg-others transition'>Get Started</Link>
                     <div className='m-10'>
-                        <i className='fa fa-search icon absolute m-3' onClick={handleSubmit} ></i>
-                        <input type='search' className=' bg-slate-100 p-2 rounded-xl text-center focus:outline-none ' placeholder='Search ...' value={searchTerm} onChange={e => setSearchTerm(e.target.value)}/>
+                        <SearchBar />
                     </div>
                 </div>
             </div>
@@ -38,5 +23,5 @@ const Landing = () => {
     )
 }
 
-export default Landing; 
+export default Landing;
 
